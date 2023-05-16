17ef5e9106b075c035357107f427aba4ff38ef09
mvreddy123 <133799159+mvreddy123@users.noreply.github.com>
2023-05-16 14:32:15 -0500

    Update streamlit_app.py
    added Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Coffee')
+streamlit.header('🥣Breakfast Menu')
+streamlit.text(' 🐔Coffee')
 streamlit.text('Bananna')
 streamlit.text('Eggs')
