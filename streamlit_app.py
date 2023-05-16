87c71453ac95c13428dcf9e95fcfbdcf31f8f622
mvreddy123 <133799159+mvreddy123@users.noreply.github.com>
2023-05-16 14:34:19 -0500

    Update streamlit_app.py
    Added Banana emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('🥣Breakfast Menu')
 streamlit.text(' 🐔Coffee')
-streamlit.text('Bananna')
+streamlit.text('🍌Bananna')
 streamlit.text('Eggs')
